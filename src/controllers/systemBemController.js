d10cb8ccc5b8079307d4bd6182ab99534c85bb95
lucas Ferreira <lucaslfr00@gmail.com>
2024-04-03 12:20:50 -0300

    estrutura do controller de bem e bemAdicionado criados

---
+++ src/controllers/systemBemController.js
@@ -0,0 +1,12 @@
+// import { prisma } from "../configs/prismaClient.js";
+// quando o prisma estiver funcionando é so descomentar
+
+class systemBemController {
+    static listarDados = (req, res) => {
+        return null
+    }
+
+    static auditarBem = (req, res) => {
+        return null
+    }
+}
