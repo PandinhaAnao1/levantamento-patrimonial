41275478f090b2df12834522f8cf21010073e2fb
lucas Ferreira <lucaslfr00@gmail.com>
2024-04-03 12:16:48 -0300

    estrutura do controller de sala e inventarios criados

---
+++ src/controllers/systemInventarioController.js
@@ -0,0 +1,12 @@
+// import { prisma } from "../configs/prismaClient.js";
+// quando o prisma estiver funcionando é so descomentar
+
+class systemInventarioController {
+    static listarInventarios = (req, res) => {
+        return null
+    }
+
+    static criarInventario = (req, res) => {
+        return null
+    }
+}
