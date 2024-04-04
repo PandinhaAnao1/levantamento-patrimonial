c1c563c2a9893a4e9668db605ba862cff45037ad
lucas Ferreira <lucaslfr00@gmail.com>
2024-04-04 18:51:24 -0300

    colocando a rota detro do try catch e colando validasão de retorno de dados

--- src/controllers/systemSalaController.js
+++ src/controllers/systemSalaController.js
@@ -1,34 +1,62 @@
 import { PrismaClient } from '@prisma/client'
+import { count } from 'console'
 const prisma = new PrismaClient()
 
 class systemSalaController {
     static listarSalas = async (req, res) => {
-        const idInventario = req.params.idInventario
-
-        const salas = await prisma.inventarios.findMany({
-            where:{
-                 inve_id: parseInt(idInventario)
-            },
-            select: {
-
-                sala_invent: {
-                    select: {
-                        sala: {
-                            select:{
-                                Sala_id: true,
-                                Sala_Nome: true
+        try{
+            const idInventario = req.params.idInventario
+    
+            if(!idInventario){
+                return res.status(400).json({
+                    error: true,
+                    code: 400,
+                    message: "id do inventario não foi fornescido"
+                });
+            }
+    
+            const salas = await prisma.inventarios.findMany({
+                where:{
+                     inve_id: parseInt(idInventario)
+                },
+                select: {
+    
+                    sala_invent: {
+                        select: {
+                            sala: {
+                                select:{
+                                    Sala_id: true,
+                                    Sala_Nome: true
+                                }
                             }
                         }
                     }
                 }
+            })
+
+            if(salas){
+                return res.status(400).json({
+                    error: true,
+                    code: 400,
+                    message: "Nem uma sala foi encontrada",
+                  });
+            }else{
+                return res.status(200).json({
+                    error: false,
+                    code: 200,
+                    message: "Usuários encontrados",
+                    data: salas
+                });
             }
-        })
-        return res.status(200).json({
-            error: false,
-            code: 200,
-            message: "Usuários encontrados",
-            data: salas
-          });
+    
+
+        }catch{
+            return res.status(500).json({
+                error: true,
+                code: 500,
+                message: "Erro interno do servido",
+              });
+        }
   
     }
 
