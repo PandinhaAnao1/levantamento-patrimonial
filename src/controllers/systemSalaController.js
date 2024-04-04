1a41de472fd0fc8c06b21ae8f21a2a3c13bf27c7
lucas Ferreira <lucaslfr00@gmail.com>
2024-04-04 15:01:55 -0300

    query de busca criada

--- src/controllers/systemSalaController.js
+++ src/controllers/systemSalaController.js
@@ -2,8 +2,34 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 class systemSalaController {
-    static listarSalas = (req, res) => {
-        return null
+    static listarSalas = async (req, res) => {
+        const idInventario = req.params.idInventario
+
+        const salas = await prisma.inventarios.findMany({
+            where:{
+                 inve_id: parseInt(idInventario)
+            },
+            select: {
+
+                sala_invent: {
+                    select: {
+                        sala: {
+                            select:{
+                                Sala_id: true,
+                                Sala_Nome: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        return res.status(200).json({
+            error: false,
+            code: 200,
+            message: "Usuários encontrados",
+            data: salas
+          });
+  
     }
 
     static listarbens = (req, res) => {
