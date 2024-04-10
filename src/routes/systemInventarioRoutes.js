7390075d4e0fd75d2be7717d02fa30b76688102f
lucas.ferreira <lucaslfr00@gmail.com>
2024-04-09 22:06:11 -0400

    Crie o arquivo de rota e controller da rota de contas

--- src/routes/systemInventarioRoutes.js
+++ src/routes/systemInventarioRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router
     .get("/inventario", systemInventarioController.listarInventarios) // Daniel criar a rota que buscar os inventarios
+    .get("/sala/:idInventario", systemInventarioController.listarSalas) //busca todas as salas pertencentes a um inventario
     //.post("/inventario", systemInventarioController.criarInventario)
+
 
 export default router;
