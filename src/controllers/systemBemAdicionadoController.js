b358906b7e10e1bec793aa8bf625cd4cbc914042
Daniel <danielpereiraestevao6@gmail.com>
2024-04-09 21:36:06 -0400

    terminado os const e de colocar os itemns dentro está faltando apenas a validacao e teste com correção de bugs

--- src/controllers/systemBemAdicionadoController.js
+++ src/controllers/systemBemAdicionadoController.js
@@ -4,25 +4,45 @@ class systemBemAdicionadoController {
 
     static adicionarBem = async (req, res) => {
         const {
-            iten_nome,
-            iten_tombo,
-            iten_descrição,
-            hist_estado_item,
-            hist_item_ocioso
+            item_add_id,
+            item_add_nome,
+            item_add_tombo,
+            item_add_descrição,
+            item_add_estado,
+            item_au_in_id,
+            item_add_imagem,
+            item_add_ocioso
           } = req.body;
         
+
      
         try {
+            // select para cada au_inve_id e sala_id  (chaves estrangeiras)
+            // tem que ter validação para todos os items 
+
             const unitExists = await prisma.bemAdicionado.create({
                 item_adicionado:{
-                    iten_nome,
-                    iten_tombo,
-                    iten_descrição, 
+                    item_add_id,
+                    item_add_nome,
+                    item_add_tombo,
+                    item_add_descrição,
+                    item_add_estado,
+                    item_au_in_id,
+                    item_add_imagem,
+                    item_add_ocioso
+
                 }
 
-            })
-
-     }   
+            });
+        return res.status(200).json(unitExists);
+     }catch(err){
+        console.error(err);
+        return res.status(500).json([{
+            error: true,
+            code: 500,
+            message: "OCORREU UM ERRO INTERNO"
+        }])
+     }  
     }
 }
 
