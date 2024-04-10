d85ad4abf489a62a130c3c21739ded430c116e45
Daniel <danielpereiraestevao6@gmail.com>
2024-04-09 20:41:59 -0400

    começando a fazer o controller foram adicionados o const com as descricoes

--- src/controllers/systemBemAdicionadoController.js
+++ src/controllers/systemBemAdicionadoController.js
@@ -3,8 +3,27 @@ const prisma = new PrismaClient()
 
 class systemBemAdicionadoController {
 
-    static adicionarBem = (req, res) => {
-        return null
+    static adicionarBem = async (req, res) => {
+        const {
+            iten_nome,
+            iten_tombo,
+            iten_descrição,
+            hist_estado_item,
+            hist_item_ocioso
+          } = req.body;
+        
+     
+        try {
+            const unitExists = await prisma.bemAdicionado.create({
+                item_adicionado:{
+                    iten_nome,
+                    iten_tombo,
+                    iten_descrição, 
+                }
+
+            })
+
+     }   
     }
 }
 
