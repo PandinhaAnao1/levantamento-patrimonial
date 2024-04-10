19e9aefee478ef968a574d736e306bac67b8c859
Daniel <danielpereiraestevao6@gmail.com>
2024-04-09 22:08:30 -0400

    validação feita faltando somente fazer o teste no postman

--- src/controllers/systemBemAdicionadoController.js
+++ src/controllers/systemBemAdicionadoController.js
@@ -11,14 +11,34 @@ class systemBemAdicionadoController {
             item_add_estado,
             item_au_in_id,
             item_add_imagem,
-            item_add_ocioso
+            item_add_ocioso,
+            item_add_sala
           } = req.body;
         
 
      
         try {
             // select para cada au_inve_id e sala_id  (chaves estrangeiras)
-            // tem que ter validação para todos os items 
+            // tem que ter validação para todos os items (FALTA VALIDAÇÃO!!!!!)
+
+            const audior_inveExists = await prisma.auditor_inventario.findFirst({
+                where:{
+                    au_in_id: item_au_in_id
+                },
+                select:{
+                    au_in_id: true
+                }
+            })
+
+            const salaExists = await prisma.sala.findFirst({
+                where:{
+                    Sala_id: item_add_sala
+                },
+                select:{
+                    item_add_sala:true
+                }
+            })
+
 
             const unitExists = await prisma.bemAdicionado.create({
                 item_adicionado:{
@@ -29,7 +49,8 @@ class systemBemAdicionadoController {
                     item_add_estado,
                     item_au_in_id,
                     item_add_imagem,
-                    item_add_ocioso
+                    item_add_ocioso,
+                    item_add_sala
 
                 }
 
