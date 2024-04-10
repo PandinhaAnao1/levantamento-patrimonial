d94e264318509f83cee955d4565e455825723cee
Adilson <adilsonjuniorpb@gmail.com>
2024-04-09 20:43:13 -0400

    rota buscar itens por sala feita

--- src/routes/systemSalaRoutes.js
+++ src/routes/systemSalaRoutes.js
@@ -1,11 +1,9 @@
 import express from "express";
 import systemSalaController from '../controllers/systemSalaController.js'
 const router = express.Router();
+router.get("/sala/:id", systemSalaController.listarbens) // busca todos os bem pertencentes a uma sala
+// .put("/sala/:idbem", systemSalaController.auditarBem) // audita um bem
 
-router
-    .get("/sala/:idInvetario", systemSalaController.listarSalas) //busca todas as salas pertencentes a um inventario
-    .get("/sala/idSala", systemSalaController.listarbens) // busca todos os bem pertencentes a uma sala
-    .put("/sala/:idbem", systemSalaController.auditarBem) // audita um bem
-
+//.get("/sala/:idInvetario", systemSalaController.listarSalas) //busca todas as salas pertencentes a um inventario
 export default router;
 
