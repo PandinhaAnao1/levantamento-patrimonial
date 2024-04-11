62293dad6830337235b304c3161ca21441519d65
Daniel <danielpereiraestevao6@gmail.com>
2024-04-10 21:51:34 -0400

    controller arurmado e feito faltando fazer o teste no post man e aguardando atualização do banco de dados para funcionar

--- src/controllers/systemBemAdicionadoController.js
+++ src/controllers/systemBemAdicionadoController.js
@@ -3,23 +3,24 @@ import { prisma } from "../configs/prismaClient.js"
 class systemBemAdicionadoController {
 
     static adicionarBem = async (req, res) => {
+        
+        console.log("pegou a funcao")
+
         const {
             item_add_id,
             item_add_nome,
-            item_add_tombo,
             item_add_descrição,
             item_add_estado,
             item_au_in_id,
             item_add_imagem,
-            item_add_ocioso,
-            item_add_sala
+            item_add_sala_id
           } = req.body;
         
 
      
         try {
             // select para cada au_inve_id e sala_id  (chaves estrangeiras)
-            // tem que ter validação para todos os items (FALTA VALIDAÇÃO!!!!!)
+            // tem que ter validação para todos os items (FALTA VALIDAÇÃO!!!!! falta a falidação do que não)
 
             const audior_inveExists = await prisma.auditor_inventario.findFirst({
                 where:{
@@ -42,16 +43,14 @@ class systemBemAdicionadoController {
 
             const unitExists = await prisma.bemAdicionado.create({
                 item_adicionado:{
-                    item_add_id,
-                    item_add_nome,
-                    item_add_tombo,
-                    item_add_descrição,
-                    item_add_estado,
-                    item_au_in_id,
-                    item_add_imagem,
-                    item_add_ocioso,
-                    item_add_sala
-
+                    item_add_id:parseInt(item_add_id),
+                    item_add_nome:parseInt(item_add_nome),
+                    item_add_estado:(item_add_estado),
+                    item_add_descricao:parseInt(item_add_descrição),
+                    item_add_au_in_id:parseInt(item_add_au_in_id),
+                    item_add_sala_id:parseInt(item_add_sala_id),
+                    item_add_imagem:parseInt(item_add_imagem),
+                    item_add_id:parseInt(item_add_id)
                 }
 
             });
