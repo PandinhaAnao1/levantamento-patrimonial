ccb1830ca66a973e296d891b4b73a35cae85b698
altenir <pandinhaAnao12@proton.me>
2024-08-12 22:25:04 -0400

    correções no midwae

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -1,29 +1,26 @@
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import messages from "./../utils/mensages.js"
 export const PRIVATE_KEY = '1010FFF'
 
 
 const AuthMiddleware = (req,res,next) => {
-
-  const [, token] = req.headers.authorization?.split(' ') || [' ', ' '];
+  
   
   
   try {
-    if(!token || token == null || token == undefined){
+    const header = req.headers['authorization'];
+  
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const token = bearer[1];
+        const payload = jwt.verify(token, PRIVATE_KEY);
+        if(payload){
+          return next();
+        }
+      }
 
-      return res.status(401).json({ message: messages.httpCodes[401]});
-    } 
-    const payload = jsonwebtoken.verify(token, PRIVATE_KEY);
-    console.log(payload);
-    const userIdFromToken = typeof payload !== 'string' && payload.user;
-
-    if(!user && !userIdFromToken) {
-      return res.send(401).json({ message: messages.httpCodes[401]});
-    }
-
-    req.headers['user'] = payload.user;
-
-    return next();
+    return res.status(401).json({ message: messages.httpCodes[401]});
+ 
   } catch(error) {
     return res.status(401).json({ message: messages.httpCodes[401]});
   }
