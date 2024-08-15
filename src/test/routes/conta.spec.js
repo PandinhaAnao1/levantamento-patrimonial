db903c3717a3a7df8b00b11a86a6d1b37fb09e02
Daniel <danielpereiraestevao6@gmail.com>
2024-08-15 19:34:20 -0400

    test finalizado e funcionando

--- src/test/routes/conta.spec.js
+++ src/test/routes/conta.spec.js
@@ -12,7 +12,7 @@ describe('contas', () => {
         expect(req.status).toBe(200)
         expect(req.body.data).toBeInstanceOf(Array)
         expect(req.body.data.length).toBeGreaterThan(0)
-        expect(req.body.message).toEqual();
+        expect(req.body.message).toEqual("Registros encontrados");
         expect(req.body.data[0].usua_id).toBeDefined()
         expect(req.body.data[0].usua_email).toBeDefined()
         expect(req.body.data[0].usua_senha).toBeDefined()
@@ -22,11 +22,15 @@ describe('contas', () => {
         
     })
 
+
+
+
+
     it("Deve retornar um objeto com os dados de apenas uma conta", async () => {
         const req = await request(app)
         .get('/contas/1')
         .set("Accept", "aplication/json")
-        expect(req.body.error.error).toEqual(false)
+        expect(req.body.error).toEqual(false)
         expect(req.status).toBe(200)
         expect(req.body.data).toBeInstanceOf(Object)
         expect(req.body.data.usua_id).toBeDefined();
