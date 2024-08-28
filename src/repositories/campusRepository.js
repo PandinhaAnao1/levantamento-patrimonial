612052be6fd7259159a1c5229812a8bea2eefc71
lucas.ferreira <lucaslfr00@gmail.com>
2024-08-28 19:51:35 -0400

    criei a estrutura basica da rotas de campus e testei

---
+++ src/repositories/campusRepository.js
@@ -0,0 +1,20 @@
+import { prisma } from "../configs/prismaClient.js"
+
+class CampusRepository{
+    static async listar(filtros){
+        return await prisma.campus.findMany(filtros);
+    }
+    
+    static async listarPorId(filtros) {
+        return await prisma.campus.findFirst(filtros)
+    }
+
+    static async criar(filtros){
+      return await prisma.campus.create(criarConta);
+    }
+
+    static async atualizar(filtros){
+      return await prisma.campus.update(atualizarcampus);
+    }
+}
+export default CampusRepository
