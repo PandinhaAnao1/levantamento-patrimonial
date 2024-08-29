015c3abcbb6dcccd593df8e783c907fd710a553f
Daniel <danielpereiraestevao6@gmail.com>
2024-08-28 21:50:21 -0400

    arrumando o filtro e alguns erros no controller,service e repository

--- src/repositories/campusRepository.js
+++ src/repositories/campusRepository.js
@@ -17,7 +17,36 @@ class CampusRepository{
       return await prisma.campus.update(atualizarcampus);
     }
 
+    static createFilterUsuario(parametros){
+      let filtro = {
+          where: {
+              ...(parametros.cidade && { cidade: {contains: parametros.cidade }}),
+              ...(parametros.telefone && { telefone: parametros.telefone }),
+              ...(parametros.nome && { nome: {contains: parametros.nome }}),
+              ...(parametros.id && { id: parametros.id }),
+              ...(parametros.bairro && { bairro: {contains: parametros.bairro }}),
+              ...(parametros.rua && { rua: {contains: parametros.rua }}),
+              ...(parametros.numero && { numero: {contains: parametros.numero }}),
 
+
+
+
+          },select:{
+            id:true,
+            senha:false,
+            nome:true,
+            rua:true,
+            bairro:true,
+            telefone:true,
+            numero:true,
+            cidade:true
+
+
+          }
+      }
+      return filtro;
+
+    }
 }
 
 
