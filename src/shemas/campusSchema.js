9cc19a59a561707b0937f7a64add90170c7eb67a
Daniel <danielpereiraestevao6@gmail.com>
2024-08-29 20:27:56 -0400

    schema criado e definido os atributos a serem passados

--- src/shemas/campusSchema.js
+++ src/shemas/campusSchema.js
@@ -12,5 +12,17 @@ class campusSchema{
     });
 
     static listarCampusPorId = z.object({id:z.number().int().min(1).positive()});
+
+
+
+    static cadastrarCampus = z.object({
+        nome: z.string().min(1).trim().max(80),
+        telefone: z.string().min(11).trim().max(80), 
+        cidade: z.string().min(1).trim().max(200),
+        bairro: z.string().min(1).trim().max(80),
+        rua: z.string().min(1).trim().max(80)
+    });
+
+
 }
 export default campusSchema;
